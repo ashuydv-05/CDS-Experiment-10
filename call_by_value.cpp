44fa65be3a444a52ec47862c57266b4f4343a71e
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-29 23:57:17 +0530

    Create call_by_value.cpp

---
+++ call_by_value.cpp
@@ -0,0 +1,32 @@
+//ashu yadav
+//23070123154
+#include <iostream>
+using namespace std;
+
+
+int a,b;
+void swap (int a, int b)
+{
+int sw;
+sw = a;
+a = b;
+b = sw;
+cout<<"Swapped Values: "<<endl;
+cout<<"a: "<<a<<endl;
+cout<<"b: "<<b<<endl;
+}
+
+int main()
+{
+int a,b;
+cout<<"Using call by value: "<<endl;
+cout<<"Enter a number: ";
+cin>>a;
+cout<<"Enter another number: ";
+cin>>b;
+cout<<"User Values: "<<endl;
+cout<<"a: "<<a<<endl;
+cout<<"b: "<<b<<endl;
+swap(a,b);
+
+}
